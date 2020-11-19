eb18213c0af1035a4f03d622e1c5e7be68af71f2
nitek29 <quentingalliou29@gmail.com>
2020-11-19 10:58:30 +0100

    base files v2

--- app/src/main/java/com/example/pasapalabra/workers/SpeechRecognizerWorker.kt
+++ app/src/main/java/com/example/pasapalabra/workers/SpeechRecognizerWorker.kt
@@ -5,15 +5,15 @@ import androidx.work.Worker
 import androidx.work.WorkerParameters
 import timber.log.Timber
 
-class SpeechRecognizerWorker (ctx: Context, params: WorkerParameters) : Worker(ctx, params){
-    override fun doWork(): Result {
-        val appContext = applicationContext
-
-        return "test"/*try {
-                // test
-            print("cc")
-        } catch (throwable: Throwable) {
-            Timber.e(throwable, "Error applying blur")
-            Result.failure()
-        }*/
-}
+//class SpeechRecognizerWorker (ctx: Context, params: WorkerParameters) : Worker(ctx, params){
+//    override fun doWork(): Result {
+//        val appContext = applicationContext
+//
+//        return "test"/*try {
+//                // test
+//            print("cc")
+//        } catch (throwable: Throwable) {
+//            Timber.e(throwable, "Error applying blur")
+//            Result.failure()
+//        }*/
+//}
